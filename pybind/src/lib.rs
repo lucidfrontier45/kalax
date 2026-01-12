f965b54e5567a95484166b3bb5a2223e3d0a7947
杜世橋 Du Shiqiao <lucidfrontier.45@gmail.com>
2026-01-12 17:06:21 +0900

    feat: add Python binding with PyO3 and maturin (#8)

---
+++ pybind/src/lib.rs
@@ -0,0 +1,26 @@
+use pyo3::prelude::*;
+
+/// A Python module implemented in Rust.
+#[pymodule]
+mod kalax {
+    use std::collections::HashMap;
+
+    use numpy::PyReadonlyArray1;
+    use pyo3::prelude::*;
+
+    #[pyfunction]
+    pub fn extract_features<'py>(
+        data: Vec<HashMap<String, PyReadonlyArray1<'py, f64>>>,
+    ) -> PyResult<Vec<HashMap<String, HashMap<String, f64>>>> {
+        let x: PyResult<Vec<HashMap<String, &[f64]>>> = data
+            .iter()
+            .map(|item| {
+                item.iter()
+                    .map(|(k, v)| Ok((k.clone(), v.as_slice()?)))
+                    .collect()
+            })
+            .collect();
+        let features = kalax_core::extract_features(&x?);
+        Ok(features)
+    }
+}

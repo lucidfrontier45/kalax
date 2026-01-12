d594bab3009e42037f1546d6b5cc2ea80f26f360
Du Shiqiao <lucidfrontier.45@gmail.com>
2026-01-12 15:20:28 +0900

    feat: add Python Binding with PyO3 Fixes #5

--- pybind/src/lib.rs
+++ pybind/src/lib.rs
@@ -1,16 +1,26 @@
-use std::collections::HashMap;
-
 use pyo3::prelude::*;
-
-#[pyfunction]
-pub fn extract_features(
-    data: &[HashMap<String, &[f64]>],
-) -> PyResult<Vec<HashMap<String, HashMap<String, f64>>>> {
-    todo!()
-}
 
 /// A Python module implemented in Rust.
 #[pymodule]
-mod pybind {
+mod kalax {
+    use std::collections::HashMap;
+
+    use numpy::PyReadonlyArray1;
     use pyo3::prelude::*;
+
+    #[pyfunction]
+    pub fn extract_features<'py>(
+        data: Vec<HashMap<String, PyReadonlyArray1<'py, f64>>>,
+    ) -> PyResult<Vec<HashMap<String, HashMap<String, f64>>>> {
+        let x: PyResult<Vec<HashMap<String, &[f64]>>> = data
+            .iter()
+            .map(|item| {
+                item.iter()
+                    .map(|(k, v)| Ok((k.clone(), v.as_slice()?)))
+                    .collect()
+            })
+            .collect();
+        let features = kalax_core::extract_features(&x?);
+        Ok(features)
+    }
 }
